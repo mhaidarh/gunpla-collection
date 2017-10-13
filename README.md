b126b763d495be81a5087dccfefde1d43128ea77
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-13 13:06:09 +0700

    Migrate data to use full API

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Simple app that displays gunpla collection.
 - Add new item to my collection
 - Remove existing item from my collection
 - Update existing item information in my collection
+
+### List
+
+From frontend, request data to backend with endpoint `/`. That response will be processed as an array. Then, loop over that array. Each item will be appended to the table body.
+
+### Add
+
+From frontend, request to add data with a payload via form.
+Get data from that from, then send that to the backend with an endpoint '/add'
--- frontend/index.js
+++ frontend/index.js
@@ -1,43 +1,11 @@
 ;(function() {
-  $.get("http://localhost:3000", data => {
-    console.log(data)
-
-    let gunplaCollection = [
-      {
-        id: 0,
-        model: "DUMMY",
-        year: 2000,
-        price: 100000
-      },
-      {
-        id: 1,
-        model: "Gundam Barbatos",
-        year: 2015,
-        price: 800000
-      },
-      {
-        id: 2,
-        model: "Gundam Unicorn",
-        year: 2010,
-        price: 1200000
-      },
-      {
-        id: 3,
-        model: "Gundam RX",
-        year: 2000,
-        price: 3200000
-      }
-    ]
-
-    // console.log(gunplaCollection)
-
-    gunplaCollection.map(item => {
+  $.get("http://localhost:3000", collection => {
+    collection.map(item => {
       const id = item.id
       const model = item.model
       const year = item.year
       const price = item.price
-      // console.log(id, model, year, price)
-
+      
       const newRow = `
       <tr>
         <th scope="row">${id}</th>
