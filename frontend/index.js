372307f8d3a0c7509959e31db64366285bc1522e
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-13 14:14:31 +0700

    Update documentation

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,12 @@
 ;(function() {
-  $.get("http://localhost:3000", collection => {
-    collection.map(item => {
+  $.get("http://localhost:3000", data => {
+    data.map(item => {
       const id = item.id
       const model = item.model
       const year = item.year
       const price = item.price
-      
+      // console.log(id, model, year, price)
+
       const newRow = `
       <tr>
         <th scope="row">${id}</th>
